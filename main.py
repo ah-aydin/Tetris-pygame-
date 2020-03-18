a74108cefdc9e1f3be8bef913673cb7c1dabf1ce
ah-aydin <ah-aydin@users.noreply.github.com>
2020-03-18 15:12:34 +0200

    Update main.py
    Added:
    - score system
    - level system
    - tweaked the next piece render

--- main.py
+++ main.py
@@ -12,9 +12,12 @@ pygame.font.init()
 game_font = pygame.font.SysFont('Game font', 30)
 rnd.seed(int(time.time())) # In order to not have the same pattern every game
 
-LEVEL = 3
-FPS = [48, 34, 24, 17, 10, 2]
+# Game variables
+LEVEL = 0
+score = 0
+FPS = [48, 43, 38, 33, 28, 23, 18, 13, 8, 6, 5, 5, 5, 4, 4, 4, 3, 3, 3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1]
 
+# Table variables
 COLUMN_COUNT = 10
 ROW_COUNT = 20
 
@@ -32,8 +35,6 @@ DRAW_OUTLINE = not DRAW_GRID # If the grid wont be drawn, each square will have 
 BLACK = (0,0,0)
 RED = (255,0,0)
 WHITE = (255,255,255)
-TURCUAZ = (64,224,208)
-LILA = (124, 234, 2 )
 
 class Piece():
 
@@ -107,45 +108,49 @@ def pos_available(piece, table):
 def get_input(piece, table):
 
     global speed
+
     keys = pygame.key.get_pressed()
+
     if keys[pygame.K_RIGHT]:
         piece.x += 1
         if not pos_available(piece, table):
             piece.x -= 1
+    
     if keys[pygame.K_LEFT]:
         piece.x -= 1
         if not pos_available(piece, table):
             piece.x += 1
+    
     if keys[pygame.K_DOWN]:
-        speed = 3
+        speed = FPS[LEVEL] // 3
+    
     if keys[pygame.K_UP]:
-        piece.rotation += 1
-        if not pos_available(piece, table):
-            piece.rotation -= 1
-
-            piece.x += 1
-            piece.rotation += 1
+        rot_lst = [(1,0), (1,1), (1,-1), (1,2), (1,-2)]
+        for rot in rot_lst:
+            piece.rotation += rot[0]
+            piece.x += rot[1]
             if not pos_available(piece, table):
-                piece.x -= 1
-                piece.rotation -=1
+                piece.rotation -= rot[0]
+                piece.x -= rot[1]
+                continue
+            break 
 
-                piece.x -= 1
-                piece.rotation += 1
-                if not pos_available(piece, table):
-                    piece.x += 1
-                    piece.rotation -= 1
+def update_score(rrc, filled_pos={}):
+    
+    global score
 
-                    piece.x += 2
-                    piece.rotation +=1
-                    if not pos_available(piece, table):
-                        piece.x -= 2
-                        piece.rotation -= 1
-
-                        piece.x -= 2
-                        piece.rotation += 1
-                        if not pos_available(piece, table):
-                            piece.x += 2
-                            piece.rotation -= 1     
+    """
+    1 Line = 50*(level + 1) points
+    2 Lines = 150*(level + 1) points
+    3 Lines = 350*(level + 1) points
+    4 Lines = 1000*(level + 1) points (aka a Tetris)
+    Clear the board = 2000*(level + 1)
+    Every piece = 10*(level + 1) points
+    """
+    score_lst = [50, 150, 350, 1000]
+    score += (score_lst[rrc-1] + rrc*100) * (LEVEL+1)
+    if len(filled_pos) == 0:
+        score += 2000 * (LEVEL+1)
 
 def remove_row(table, row_to_remove, filled_pos):
 
@@ -167,8 +172,9 @@ def remove_row(table, row_to_remove, filled_pos):
                 filled_pos[(col, row)] = table[row][col]
     return table, filled_pos
 
-def check_lines(table, filled_pos):
+def check_lines(table, filled_pos, removed_row_count):
 
+    rrc = 0
     for row in range(ROW_COUNT):
         full_row = True
         for col in range(COLUMN_COUNT):
@@ -177,7 +183,11 @@ def check_lines(table, filled_pos):
                 break
         if full_row:
             table, filled_pos = remove_row(table, row, filled_pos)
-    return table, filled_pos
+            rrc += 1
+    if rrc != 0:
+        update_score(rrc, filled_pos)
+    removed_row_count += rrc
+    return table, filled_pos, removed_row_count, rrc
 
 def is_game_over(row):
     for color in row:
@@ -205,9 +215,9 @@ def draw_play_area(surface):
         for y in range(19):
             pos_y += BLOCK_SIZE
             pygame.draw.line(surface, WHITE, (DELTA, pos_y), (WIDTH - DELTA, pos_y))
-    pygame.draw.line(surface, LILA, (DELTA, 0), (DELTA, PLAY_HEIGHT), 4)
-    pygame.draw.line(surface, LILA, (WIDTH - DELTA, 0), (WIDTH - DELTA, PLAY_HEIGHT), 4)
-    pygame.draw.line(surface, LILA, (DELTA, PLAY_HEIGHT), (WIDTH - DELTA, PLAY_HEIGHT), 4)
+    pygame.draw.line(surface, RED, (DELTA, 0), (DELTA, PLAY_HEIGHT), 4)
+    pygame.draw.line(surface, RED, (WIDTH - DELTA, 0), (WIDTH - DELTA, PLAY_HEIGHT), 4)
+    pygame.draw.line(surface, RED, (DELTA, PLAY_HEIGHT), (WIDTH - DELTA, PLAY_HEIGHT), 4)
 
 def draw_table(surface, table):
     
@@ -227,10 +237,23 @@ def draw_table(surface, table):
 
 def text_render(surface):
     
+    global score, LEVEL
+
     next_piece_text = "Next piece:"
-    size = game_font.size(next_piece_text)
+    score_text = "Score: " + str(score)
+    level_text = "Level: " + str(LEVEL)
+
+    size_p = game_font.size(next_piece_text)
+    size_s = game_font.size(score_text)
+    size_l = game_font.size(level_text)
+
     next_piece_text_render = game_font.render(next_piece_text, True, WHITE)
-    surface.blit(next_piece_text_render, (WIDTH - DELTA//2 - size[0]//2, HEIGHT//3))
+    score_text_render = game_font.render(score_text, True, WHITE)
+    level_text_render = game_font.render(level_text, True, WHITE)
+    
+    surface.blit(next_piece_text_render, (WIDTH - DELTA//2 - size_p[0]//2, HEIGHT//3))
+    surface.blit(score_text_render, (DELTA//50, 10 + size_l[1]))
+    surface.blit(level_text_render, (DELTA//50, 10 + size_l[1] + size_s[1]+2))
 
 def update_display(surface, current_piece, next_piece, table):
 
@@ -247,15 +270,18 @@ def update_display(surface, current_piece, next_piece, table):
 
 def main(surface):
 
-    global game_over, speed, filled_pos
+    global game_over, speed, filled_pos, LEVEL
     filled_pos = {}
     speed = 1
     game_over = False
 
     current_piece = get_piece()
     next_piece = get_piece()
-    next_piece.x = 13
-    next_piece.y = 10
+    next_piece_clone = Piece(550//BLOCK_SIZE, (HEIGHT//3+125)//BLOCK_SIZE, next_piece.shape)
+    next_piece_clone.rotation += 1
+    #next_piece.x = 13
+    #next_piece.y = 10
+    row_remove_count = 0
     clock = pygame.time.Clock()
     frame = 0
 
@@ -276,17 +302,29 @@ def main(surface):
         table = get_table(filled_pos)
         if is_game_over(table[0]):
             game_over = True
-        table, filled_pos = check_lines(table, filled_pos)
-        if frame >= FPS[LEVEL] // speed: # After every FPS[LEVEL]'th frame move the piece downwards
-            if drop_piece(current_piece, table) == False: # If the piece cannot be moved then get the next one
-                next_piece.x = 5
-                next_piece.y = 0
+        
+        rrc = 0
+        table, filled_pos, row_remove_count, rrc = check_lines(table, filled_pos, row_remove_count)
+
+        # Update the level
+        if row_remove_count >= 10:
+            row_remove_count = row_remove_count % 10
+            LEVEL = min(LEVEL+1, len(FPS)-1)
+        
+        # After every FPS[LEVEL]'th frame move the piece downwards
+        if frame >= FPS[LEVEL] // speed:
+            # If the piece cannot be moved then get the next one
+            if drop_piece(current_piece, table) == False:
+                #next_piece.x = 5
+                #next_piece.y = 0
                 current_piece = next_piece
                 next_piece = get_piece()
-                next_piece.x = 13
-                next_piece.y = 10
+                next_piece_clone = Piece(550//BLOCK_SIZE, (HEIGHT//3+125)//BLOCK_SIZE, next_piece.shape)
+                next_piece_clone.rotation += 1
+                #next_piece.x = 13
+                #next_piece.y = 10
             frame = 0
-        update_display(surface, current_piece, next_piece, table)
+        update_display(surface, current_piece, next_piece_clone, table)
 
 def main_menu():
     
@@ -310,6 +348,8 @@ def main_menu():
     
     # Run the game
     main(window)
+
+    # game_over_menu(window)
 
 if __name__ == '__main__':
     main_menu()
